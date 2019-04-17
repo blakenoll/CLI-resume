ca254de6b5193f1b3e560318c6a572411f7496b1
Blake Noll <nollblake@gmail.com>
2019-04-17 14:23:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+"use strict";
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const fetch = require("node-fetch");
+const wrapText = require("./wrapText.js");
+const version = require("./package.json").version;
+const resume = require("./resume.json");
+
+const green = chalk.bold.green;
+const blue = chalk.bold.underline.blueBright;
+const cyan = chalk.cyan;
+const dimWhite = chalk.dim.white;
+const white = chalk.white;
+
+const resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+  console.log("Hello, My name is Blake Noll and welcome to my resume");
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    const option = answer.resumeOptions;
+    console.log(green("---------------------------------------------"));
+    resume[`${option}`].forEach(info => {
+      console.log(
+        green("|   => " + blue(info.title) + " " + white(info.content))
+      );
+      if (info.description) {
+        wrapText(info.description, 50, text => {
+          console.log(green("|      " + dimWhite(text)));
+        });
+      }
+    });
+    console.log(green("---------------------------------------------"));
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+fetch("https://registry.npmjs.org/gnarly-graphql")
+  .then(res => res.json())
+  .then(json => {
+    const currentVersion = json["dist-tags"].latest;
+    // check for new version
+    if (currentVersion !== version) {
+      console.log("");
+      console.log(green("---------------------------------------------"));
+      console.log(
+        "Update available! " +
+          dimWhite(`${version}`) +
+          " => " +
+          green(`${currentVersion}`)
+      );
+      console.log(
+        white("Run: ") + cyan("npm i -g blakeNollResume") + white(" to upgrade")
+      );
+      console.log(green("---------------------------------------------"));
+    } else {
+      main();
+    }
+  })
+  .catch(() => {
+    // if fetch fails run main()
+    main();
+  });
